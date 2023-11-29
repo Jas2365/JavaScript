d4585e5355f665cfadbccade62ed291e8ed19743
Jas <jastha6wdc@gmail.com>
2023-11-29 14:25:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,212 @@
+// //lag a statement using console.log()
+// console.log("hello from main.js")
+
+
+// // variable
+// let age = 25
+// console.log(age)
+
+// const salary = 0
+// // salary = 80000
+// console.log(salary)
+
+// let sum
+// sum = 5
+// console.log(sum)
+
+// const name = "jagjeet"
+// const language = '`avaScript'`
+// const total = 0
+// const PI = 3.14
+
+// const IsPrimaryNumber = true
+// const IsNewUser = false
+
+// let result
+// console.log(result)
+
+// const res = null
+
+// const data = null
+
+// const person = {
+//   firstName: 'Bruce',
+//   lastName: 'Wayne',
+//   age: 30,
+// }
+
+// console.log(person.firstName)
+
+// const oddNumbers = [1,3,5,7,9]
+// console.log(oddNumbers[2])
+
+// let a = 10
+// a = 'jagjeet'
+// a = true
+
+// // console.log(a)
+
+// let x = 10
+// let y = 5 
+
+// console.log(x%y)
+// console.log(++x)
+// console.log(--y)
+
+// console.log(x < y)
+
+// const isValidNumber = x > 20 || 8 > y
+// console.log(isValidNumber)
+
+// const isValid = false
+// console.log(!isValid)
+
+// console.log('Bruce ' + 'Wayne')
+
+// const isEven = 10 % 2 === 0 ? 'Number is even' : 'Number is odd'
+// console.log(isEven)
+
+// console.log(true + '3')
+// console.log('4' - '2')
+// console.log('4' * '6')
+// console.log('6' / '3')
+// console.log('bruce' - 'wayne')
+// console.log('5' - null)
+// console.log(5 + undefined)
+
+// console.log(parseFloat('3.14'))
+// console.log((500).toString())
+// console.log(Boolean(10))
+// console.log(Boolean(''))
+// console.log(Boolean(null))
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean(NaN))
+
+// const var1 = null
+// const var2 = undefined
+
+// console.log(var1 == var2)
+// console.log(var1 === var2)
+
+// const num = 0
+
+// if (num > 0) {
+//   console.log('Number is positive')
+// } else if (num < 0) {
+//   console.log('Number is negative')
+// } else {
+//   console.log('Number is zero')
+// }
+
+// const color = 19
+
+// switch(color) {
+//   case 'red' : console.log('Color is red')
+//   break
+//   case 'blue' : console.log('Color is blue')
+//   break
+//   case 'green' : console.log('Color is green')
+//   break
+//   default : console.log('Not a valid color')
+// }
+
+// for(let i = 1; i <= 5; i++) {
+//   console.log('Iteration number ' + i)
+// }
+
+// let i = 1
+// while (i <= 5) {
+//   console.log('Iteration number ' + i)
+//   i++
+// }
+
+// let i = 1
+// do {
+//   console.log('Iteration number ' + i)
+//   i++
+// } while (i <= 5)
+
+// const numArray = [1,2,3,4,5]
+
+// for (const num of numArray) {
+//   console.log('Iteration number ' + num)
+// }
+
+// function greet(username) {
+//   console.log('Good morning ' + username)
+// }
+
+// greet('bruce')
+// greet('clark')
+// greet('diana')
+
+// function add(a,b) {
+//   return a + b
+// }
+// const arrowSum = (a, b) => a + b
+// const addFive = num => num + 5
+
+//  sum = addFive(25)
+// console.log(sum)
+
+// const myNum = 100
+// const myName = 'Superman'
+
+// if(true) {
+//   const myName = 'Jagjeet'
+//   console.log(myName)
+//   console.log(myNum)
+// }
+
+// function testfn() {
+//   const myName = 'Batman'
+//   console.log(myName)
+//   console.log(myNum)
+// }
+
+// testfn()
+// ========================================================================================
+// const isEven = 10 % 2 === 0 ? 'Number is even' : 'Number is odd'
+// console.log(isEven)
+
+// console.log(true + '3')
+// console.log('4' - '3')
+// console.log('Bruce ' - 'Wayne')
+// console.log('2' - null)
+// console.log(5 + undefined)
+
+// console.log(parseFloat('3.14'))
+// console.log((500).toString())  // not work on null and undefined
+// console.log(Boolean(23)) // null undefiened 0 '' null === false
+
+// const var1 = false
+// const var2 = ''
+
+// console.log(var1 == var2);
+// console.log(var1 === var2);
+
+// const num = 0
+
+// if (num > 0) {
+//   console.log('Number is positive');
+// } else if (num < 0) {
+//   console.log('Number is negetive');
+// } else {
+//   console.log('Number is zero');
+// }
+
+// const color = 10
+
+// switch(color) {
+//   case 'red' :
+//     console.log('Color is red')
+//     break
+//     case 'blue' : 
+//     console.log('Color is blue')
+//     break
+//     case 'green' :
+//     console.log('Color is green')
+//     default: 
+//     console.log('Not a valid color')
+// }
